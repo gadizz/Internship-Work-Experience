e5813ce857bc0f4914139a1bd5ae8ab2e3a73e09
gadizz <gadizzreal@gmail.com>
2025-08-23 20:49:35 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,1 +1,30 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
+app = Flask(__name__)
+
+# Database setup (SQLite file will be tasks.db in project folder)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+# Define Task model
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    done = db.Column(db.Boolean, default=False)
+
+# Home route
+@app.route('/')
+def home():
+    return render_template("home.html")
+
+# Route to display tasks
+@app.route('/tasks')
+def show_tasks():
+    tasks = Task.query.all()  # get all tasks from DB
+    return render_template("tasks.html", tasks=tasks)
+
+if __name__ == '__main__':
+    app.run(debug=True)
